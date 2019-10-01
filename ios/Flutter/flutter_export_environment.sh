8f2887f8d665a2e6646b83ed5d428a7434463948
“Keita <b1017172@fun.ac.jp>
2019-10-01 17:11:51 +0900

    sample

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/micad/Develop/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/micad/Develop/FlutterProjects/kyushoku_sample/sample"
-export "FLUTTER_TARGET=/Users/micad/Develop/FlutterProjects/kyushoku_sample/sample/lib/screen/main.dart"
+export "FLUTTER_ROOT=/Users/keita1024/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/keita1024/Downloads/Civic_Tech-develop"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/micad/Develop/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/keita1024/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
--- lib/screen/monthlyMenu.dart
+++ lib/screen/monthlyMenu.dart
@@ -26,38 +26,169 @@ class _MonthlyMenuState extends State<MonthlyMenu> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
+  Widget build(BuildContext context) {//画面の設定
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(16.0),
-        child: CalendarCarousel<Event>( // カレンダー部分
-          onDayPressed: onDayPressed, // 日にちが選択された時の処理
-          showOnlyCurrentMonthDate: true, // 一ヶ月のみ表示
-          showHeader: false,  // ヘッダー(xx年yy月)を隠す
-          weekendTextStyle: TextStyle(
-            // 土日の色変更
-            color: Colors.red,
-          ),
-
-          /* 今日の処理をなくす */
-          todayBorderColor: null,
-          todayButtonColor: null,
-          todayTextStyle: TextStyle(
-            color: Colors.black,
-          ),
-
-          /* 選択した日の処理 */
-          selectedDayBorderColor: Color.fromARGB(0, 0, 0, 255), // 無色(nullだとエラー出る)
-          selectedDayButtonColor: Theme.of(context).primaryColor,
-
-          selectedDateTime: _currentDate, // 初期値として選択する日にち(今日を指定)
-          locale: 'JA', // 日本のカレンダーに設定
-          daysHaveCircularBorder: true, // 枠線を丸く設定
-          customGridViewPhysics:
-          NeverScrollableScrollPhysics(), // カレンダーの縦スクロールを固定
+        appBar: new AppBar(
+          title: new Text('カレンダー画面'),
         ),
-      ),
+        body: Container(
+          margin: const EdgeInsets.all(16.0),
+          child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              _calenderScreen(context),
+              _schoollunchScreen(),
+            ],
+          ),
+        )
     );
   }
+
+  Widget _calenderScreen(BuildContext context) {//カレンダーの画面
+    final Size size = MediaQuery.of(context).size;//画面のサイズを取得する
+    return Container(
+      child: CalendarCarousel<Event>( // カレンダー部分
+            onDayPressed: onDayPressed, // 日にちが選択された時の処理
+            showOnlyCurrentMonthDate: true, // 一ヶ月のみ表示
+            showHeader: false,  // ヘッダー(xx年yy月)を隠す
+            weekendTextStyle: TextStyle(
+              // 土日の色変更
+              color: Colors.red,
+            ),
+
+            //カレンダーの高さ
+            height: size.height * 1/2,
+
+            /* 今日の処理をなくす */
+            todayBorderColor: null,
+            todayButtonColor: null,
+            todayTextStyle: TextStyle(
+              color: Colors.black,
+            ),
+
+            /* 選択した日の処理 */
+            selectedDayBorderColor: Color.fromARGB(0, 0, 0, 255), // 無色(nullだとエラー出る)
+            selectedDayButtonColor: Theme.of(context).primaryColor,
+
+            selectedDateTime: _currentDate, // 初期値として選択する日にち(今日を指定)
+            locale: 'JA', // 日本のカレンダーに設定
+            daysHaveCircularBorder: true, // 枠線を丸く設定
+            customGridViewPhysics:
+            NeverScrollableScrollPhysics(), // カレンダーの縦スクロールを固定
+          ),
+        );
+  }
+
+  Widget _schoollunchScreen(){//下の献立表を出力する画面
+    // List<String> listDish = [];//メニューを格納するリスト
+//    for (var i = 0; i < todayDish.length; i++) {//todayDishから順番に料理名を取り出しlistDishに格納している
+//      listDish.add(todayDish[i].dishName);
+//    }
+//    if(listDish.length < 6){//最大6品なので5品目しかない時に6個目を追加!ここのところをもう少しスマートに描きたい...
+//      listDish.add("");
+//    }
+    return Container(
+        margin: EdgeInsets.all(16.0),
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(//○月○日の献立と表示させるためのboxの設定
+              width: 500.0,
+              height: 50.0,
+              child:  RaisedButton(//ボタン
+                onPressed: () {
+                  Navigator.pushNamed(context, '/details');
+                },
+                child:  Text(
+                  'その日の献立',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize:16.0,
+                  ),
+                ),
+              ),
+            ),
+            Container(//主食のメニュー
+              margin: EdgeInsets.only(left:50.0,top:20.0),
+              child: Row(
+                  children:<Widget>[
+                    Text(
+                      '主食   ',//ここら辺を変える必要がある
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize:20.0,
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            'ご飯',//ここら辺をmenuとかに置き換える？
+                            style: TextStyle(
+                              fontSize:16.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]
+              ),
+            ),
+            Container(//副食のメニュー・もう少し中をスマートに描きたい...
+              margin: EdgeInsets.only(left:50.0),
+              child: Row(
+                  children:<Widget>[
+                    Text(
+                      '副食   ',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize:20.0,
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            'おかず1',//ここら辺をmenuとかに置き換える？
+                            style: TextStyle(
+                              fontSize:16.0,
+                            ),
+                          ),
+                          Text(
+                            'おかず2',//ここら辺をmenuとかに置き換える？
+                            style: TextStyle(
+                              fontSize:16.0,
+                            ),
+                          ),
+                          Text(
+                            'おかず3',//ここら辺をmenuとかに置き換える？
+                            style: TextStyle(
+                              fontSize:16.0,
+                            ),
+                          ),
+                          Text(
+                            'おかず4',//ここら辺をmenuとかに置き換える？
+                            style: TextStyle(
+                              fontSize:16.0,
+                            ),
+                          ),
+                          Text(
+                            'おかず5',//ここら辺をmenuとかに置き換える？
+                            style: TextStyle(
+                              fontSize:16.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]
+              ),
+            ),
+          ],
+        )
+    );
+  }
+
 }
