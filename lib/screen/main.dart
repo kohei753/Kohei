6464907ce575ebf36756e998766059bc896ebc0d
Micad <b1017173@fun.ac.jp>
2019-10-03 16:11:46 +0900

    きしかようの仮プッシュ

--- lib/screen/main.dart
+++ lib/screen/main.dart
@@ -6,7 +6,7 @@ import 'package:sample/screen/splash.dart';
 void main() {
   runApp(
     MaterialApp(
-      title: '給食アプリ(仮)',
+      title: '給食アプリ(仮)',  // HELP  Good name
       theme: ThemeData(
         primaryColor: Colors.deepOrangeAccent,
         // primaryColor: Color(0xFFe1a067), // 一応チームカラー(汚かった)
--- lib/screen/setting.dart
+++ lib/screen/setting.dart
@@ -10,6 +10,47 @@ class Setting extends StatefulWidget {
 }
 
 class _SettingState extends State<Setting> {
+  /* 表示するリスト */
+  final List<Widget> _itemList = [
+    ListTile(
+      contentPadding: EdgeInsets.symmetric(horizontal: 12.0),
+      leading: Container(
+        child: Icon(
+          Icons.person,
+          size: 50,
+          color: Colors.white,
+        ),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.black26,
+        ),
+      ),
+      title: Text(
+        '登録情報',
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.black,
+        ),
+      ),
+      subtitle: Text('巴中学校'),
+      trailing: Icon(Icons.keyboard_arrow_right),
+      onTap: () {},
+    ),
+    ListTile(),
+  ];
+
+  /* ボディのリスト生成 */
+  Widget _buildBodyList() {
+    return ListView.separated(
+      padding: EdgeInsets.zero,
+      itemCount: _itemList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return _itemList[index];
+      },
+      separatorBuilder: (BuildContext context, int index) => Divider(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -17,6 +58,7 @@ class _SettingState extends State<Setting> {
       appBar: AppBar(
         title: Text('設定'),
       ),
+      body: _buildBodyList(),
     );
   }
 }
