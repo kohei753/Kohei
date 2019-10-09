e7f9f0c0963f2ae0cb5abdd2abe7398bd5cc74fa
KishikaMikasa <b1017037@fun.ac.jp>
2019-10-09 17:52:36 +0900

    [feat]スクロールできるようにした

--- lib/screen/information.dart
+++ lib/screen/information.dart
@@ -17,62 +17,60 @@ class _InformationState extends State<Information> {
         title: Text('インフォメーション'), // タイトル
       ),
       body: Center(
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'このアプリについて',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                height: 2.0,
-                fontSize: 22,
+        child: SingleChildScrollView(
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'このアプリについて',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  height: 2.0,
+                  fontSize: 22,
+                ),
               ),
-            ),
-            Text(
-              '　オープンデータと函館市の小中学校からいただいたデータを使っています。',
-              style: TextStyle(
-                height: 1.0,
-                fontSize: 18,
+              Text(
+                '　オープンデータと函館市の小中学校からいただいたデータを使っています。',
+                style: TextStyle(
+                  height: 1.0,
+                  fontSize: 18,
+                ),
               ),
-            ),
-            Text(
-              '使っているオープンデータ',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                height: 2.0,
-                fontSize: 20,
+              Text(
+                '使っているオープンデータ',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  //height: 2.0,
+                  //height: 2.0,
+                  fontSize: 20,
+                ),
               ),
-            ),
-            Text(
-              '・函館の給食の放射能検査について''https://www.city.hakodate.hokkaido.jp/docs/2014031600182/',
-              style: TextStyle(
-                height: 1.0,
-                fontSize: 18,
+              Text(
+                '・函館の給食の放射能検査について\n''https://www.city.hakodate.hokkaido.jp/docs/2014031600182/\n'
+                    '・函館の給食の原産地について\n''https://www.city.hakodate.hokkaido.jp/docs/2014031600120/\n'
+                    '・函館市の学校給食　簡単メニュー紹介\n''https://www.city.hakodate.hokkaido.jp/docs/2016012500108/\n',
+                style: TextStyle(
+                  //height: 1.0,
+                  fontSize: 18,
+                ),
               ),
-            ),
-            Text(
-              '・函館の給食の原産地について''https://www.city.hakodate.hokkaido.jp/docs/2014031600120/',
-              style: TextStyle(
-                height: 1.0,
-                fontSize: 18,
+              Text(
+                '製作者の連絡先 \n xxx@xxx.jp',
+                style: TextStyle(
+                  height: 2.0,
+                  fontSize: 18,
+                ),
               ),
-            ),
-            Text(
-              '・もうちょっと',
-              style: TextStyle(
-                height: 1.0,
-                fontSize: 18,
+              Text(
+                '協力してくださった方',
+                style: TextStyle(
+                  height: 1.0,
+                  fontSize: 18,
+                ),
               ),
-            ),
-            Text(
-              '製作者の連絡先',
-              style: TextStyle(
-                height: 2.0,
-                fontSize: 18,
-              ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        )
       ),
     );
   }
