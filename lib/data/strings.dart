6f4165fa21dbd0e0da0366d0d65cc189695c1342
Micad <b1017173@fun.ac.jp>
2019-10-14 01:19:54 +0900

    [feat]利用データについての項目を追加

---
+++ lib/data/strings.dart
@@ -0,0 +1,20 @@
+/* アプリ内で使う文字列のストレージ */
+class InfoStrings {
+  static const String dataTitle = '利用データについて';
+  static const String dataLocation =
+      '　本サービスは, 函館市が公開しているオープンデータと, 協力をいただいた函館市の小中学校から提供されたデータもとに作成しています.';
+  static const String dataAttention =
+      '　また, これらのデータは正確性が保証されておらず, サービス内の情報によるトラブル・事故等において, 本サービスの製作者及び関係団体は一切責任を負いません. 正確な情報は, 函館市役所及び各学校の担当職員へ問い合わせください.';
+  static const String openData = 'オープンデータ';
+  static const String linkInternalAffairsCommunications = '総務省 地方公共団体のオープンデータの推進';
+  static const String usingData = '利用中のデータ(敬称略)';
+  static const String noOpenData = '現在オープンデータを使用しておりません.';
+  static const String illustration1 = 'イラストレイン';
+  static const String illustration2 = 'ICOOON MONO';
+  static const String cooperationSchools = '協力していただいている学校';
+  static const List<String> cooperationSchoolsValue = [
+    '巴中学校',
+  ];
+
+  static const String personalTitle = '個人情報の扱い';
+}
--- lib/screen/information.dart
+++ lib/screen/information.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart'; // マテリアルデザインしようぜのやーつ
 
+import 'package:url_launcher/url_launcher.dart';
+
+import 'package:sample/data/strings.dart';
+
 /* インフォメーション画面 */
 class Information extends StatefulWidget {
   @override
@@ -7,73 +11,145 @@ class Information extends StatefulWidget {
 }
 
 class _InformationState extends State<Information> {
+  final double head = 25.0; // 大見出し
+  final double middleHead = 20.0; // サブ見出し
+  final double normal = 16.0; // 本文
+  final double lineSpace = 8.0; // 行間
+
+  /* Bodyウィジェット */
+  Widget _buildBody() {
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _dataText(), // 利用データについて
+          SizedBox(height: lineSpace),
+          _personalDataText(), // 個人情報について
+        ],
+      ),
+    );
+  }
+
+  /* テキストの汎用クラス */
+  Widget _textFormat(
+      {@required String text, double fontSize: 16.0, Color color}) {
+    if (fontSize == head) {
+      return Text(
+        text,
+        style: TextStyle(
+          fontSize: fontSize,
+          color: color,
+          decoration: TextDecoration.underline,
+          decorationStyle: TextDecorationStyle.dashed,
+          decorationColor: Theme.of(context).primaryColor,
+        ),
+      );
+    } else {
+      return Text(
+        text,
+        style: TextStyle(
+          fontSize: fontSize,
+          color: color,
+        ),
+      );
+    }
+  }
+
+  /* 利用しているデータについて */
+  Widget _dataText() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        _textFormat(text: InfoStrings.dataTitle, fontSize: head),
+        SizedBox(height: lineSpace),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0), // 周りの余白
+          child: _textFormat(
+            text: InfoStrings.dataLocation + '\n' + InfoStrings.dataAttention,
+          ),
+        ),
+        SizedBox(height: lineSpace),
+        _textFormat(text: InfoStrings.openData, fontSize: middleHead),
+        SizedBox(height: lineSpace),
+        Image.asset('assets/open_data.png'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            InkWell(
+              onTap: () async {
+                const url =
+                    'http://www.soumu.go.jp/menu_seisaku/ictseisaku/ictriyou/opendata/';
+                if (await canLaunch(url)) {
+                  await launch(url);
+                }
+              },
+              child: _textFormat(
+                text: InfoStrings.linkInternalAffairsCommunications,
+                fontSize: 8.0,
+                color: Theme.of(context).accentColor,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: lineSpace),
+        _textFormat(text: InfoStrings.usingData, fontSize: middleHead),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: lineSpace),
+          child: Column(
+            children: <Widget>[
+              InkWell(
+                onTap: () async {
+                  const url = 'https://illustrain.com/';
+                  if (await canLaunch(url)) await launch(url);
+                },
+                child: _textFormat(
+                    text: InfoStrings.illustration1,
+                    color: Theme.of(context).accentColor),
+              ),
+              InkWell(
+                onTap: () async {
+                  const url = 'https://icooon-mono.com/';
+                  if (await canLaunch(url)) await launch(url);
+                },
+                child: _textFormat(
+                    text: InfoStrings.illustration2,
+                    color: Theme.of(context).accentColor),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: lineSpace),
+        _textFormat(text: InfoStrings.cooperationSchools, fontSize: middleHead),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: lineSpace),
+          child: Column(
+            children: <Widget>[
+              _textFormat(text: InfoStrings.cooperationSchoolsValue[0]),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  /* 個人情報について */
+  Widget _personalDataText() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        _textFormat(text: InfoStrings.personalTitle, fontSize: head),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('インフォメーション'), // タイトル
+        title: Text('このアプリについて'), // タイトル
       ),
-      body: Center(
-        child: SingleChildScrollView( //スクロールできるようにする
-          child: Column(
-            children: <Widget>[
-              Text(
-                'このアプリについて',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  height: 2.0,
-                  fontSize: 22,
-                ),
-              ),
-              Text(
-                '　オープンデータと函館市の小中学校からいただいたデータを使っています。',
-                style: TextStyle(
-                  height: 1.0,
-                  fontSize: 18,
-                ),
-              ),
-              Text(
-                '使っているオープンデータ',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  height: 2.0,
-                  fontSize: 20,
-                ),
-              ),
-              Text(
-                '・函館の給食の放射能検査について\n''https://www.city.hakodate.hokkaido.jp/docs/2014031600182/\n'
-                    '・函館の給食の原産地について\n''https://www.city.hakodate.hokkaido.jp/docs/2014031600120/\n'
-                    '・函館市の学校給食　簡単メニュー紹介\n''https://www.city.hakodate.hokkaido.jp/docs/2016012500108/\n',
-                style: TextStyle(
-                  //height: 1.0,
-                  fontSize: 18,
-                ),
-              ),
-              Text(
-                '製作者の連絡先',
-                style: TextStyle(
-                  height: 2.0,
-                  fontSize: 20,
-                ),
-              ),
-              Text(
-                'xxx@xxx.jp',
-                style: TextStyle(
-                  height: 1.0,
-                  fontSize: 18,
-                ),
-              ),
-              Text(
-                '協力してくださった方',
-                style: TextStyle(
-                  height: 2.0,
-                  fontSize: 20,
-                ),
-              ),
-            ],
-          ),
-        )
-      ),
+      body: _buildBody(),
     );
   }
 }
