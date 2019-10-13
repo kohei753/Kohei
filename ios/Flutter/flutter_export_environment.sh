1b3f326c1a88ecb5750b422dd50adc8787a6aa85
“Keita <b1017172@fun.ac.jp>
2019-10-14 01:11:42 +0900

    [merge]正隆のマージしたdevelopをpullした

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-<<<<<<< HEAD
-export "FLUTTER_ROOT=/Users/keita1024/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/keita1024/AndroidStudioProjects/test_app"
-export "FLUTTER_TARGET=lib/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/keita1024/development/flutter/bin/cache/artifacts/engine/ios"
-=======
 export "FLUTTER_ROOT=/Users/kudoumasataka/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/kudoumasataka/Desktop/flutter/Civic_Tech"
 export "FLUTTER_TARGET=/Users/kudoumasataka/Desktop/flutter/Civic_Tech/lib/screen/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/kudoumasataka/flutter/bin/cache/artifacts/engine/ios"
->>>>>>> 87563e0dfbe7ad7e3362bf0a199e34803958dd5f
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
