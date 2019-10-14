220ce8ab7763ad59625effd27a3761532d73df89
Micad <b1017173@fun.ac.jp>
2019-10-15 01:47:42 +0900

    [style]デバッグ用のコードを消去

--- lib/screen/entry.dart
+++ lib/screen/entry.dart
@@ -4,9 +4,6 @@ import 'package:sample/data/menu.dart';
 import 'package:sample/data/dri.dart';
 import 'secondEntry.dart';
 
-import 'home.dart';
-import 'package:sample/data/child.dart';
-
 /* 初期登録画面 */
 class Entry extends StatefulWidget {
   final Map<DateTime, Menu> menus;
@@ -203,8 +200,7 @@ class _EntryState extends State<Entry> {
         MaterialPageRoute(
           settings: RouteSettings(name: '/secondEntry'),
           builder: (BuildContext context) =>
-              // SecondEntry(menus: menus, dri: dri, name: name, sex: gender),
-          Home(menus: menus, dri: dri, child: Child('けいた', '中学校', 8, 1), selectDay: DateTime(2019, 8, 19))
+              SecondEntry(menus: menus, dri: dri, name: name, sex: gender),
         ));
   }
 
